a3d9571027b2bcec06264e6c3cee960613e7562b
Aleksandr Petrushkin <petrushkin@plarin.net>
2022-01-27 00:43:54 +0300

    feat: add fabric class

--- 3-Fabric/factory_method.py
+++ 3-Fabric/factory_method.py
@@ -16,24 +16,30 @@ class Point:
     #     elif system == CoordinateSystem.POLAR:
     #         self.x = a * cos(b)
     #         self.y = a * sin(b)
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
     def __str__(self):
         return f'x:{self.x} y:{self.y}'
 
-    @staticmethod
-    def new_cartesian_point(x, y):
-        return Point(x, y)
+    # Когда фабричных методов становится слишком много можно сгруппировать их в отдельную сущность
+    class PointFactory:
+        def new_cartesian_point(self, x, y):
+            point = Point(x, y)
+            point.x = x
+            point.y = y
+            return point
 
-    @staticmethod
-    def new_polar_point(rho, theta):
-        return Point(rho * cos(theta), rho * sin(theta))
+        def new_polar_point(self, rho, theta):
+            return Point(rho * cos(theta), rho * sin(theta))
+
+    factory = PointFactory()
+    # Фабрика - все что угодно что создает объекты
 
 
 if __name__ == '__main__':
     p = Point(2, 3)
-    p2 = Point.new_polar_point(5, 6)
+    p2 = Point.factory.new_polar_point(3, 4)
     print(p)
     print(p2)
